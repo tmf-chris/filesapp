c7519c5ef4896c709d04c97e5e250437d20201b1
Chris Cousins <chris@addapp.io>
2018-08-16 19:20:17 -0300

    didnt add trait or dates stuff into file model for softdeletesgit add .

--- server/app/File.php
+++ server/app/File.php
@@ -4,13 +4,16 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 use \App\Traits\Uuids;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class File extends Model
 {
     use Uuids;
+    use SoftDeletes;
 
     public $incrementing = false;
     protected $table = 'files';
     protected $hidden = ['path'];
     protected $fillable = ['id', 'name', 'path', 'type', 'size', 'created_at', 'updated_at'];
+    protected $dates = ['deleted_at'];
 }
