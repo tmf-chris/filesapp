e4530c1e5153bf11992b111217eb518132743314
Chris Cousins <chris@addapp.io>
2018-08-16 04:52:39 -0300

    small rename of test, removed snapshot

--- client/__test__/ActionButton.test.js
+++ client/__test__/ActionButton.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ActionButton from '../src/js/features/dialogs/components/ActionButton';
 
-test('Action button', () => {
+test('Action button click calls supplied callback', () => {
     let actionDone = false;
     const action = () => {
         actionDone = true;
